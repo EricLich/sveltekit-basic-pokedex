1039819476640754dee408097e45d63053017d76
Eric Lich <eric.lichtensztein@aleia.io>
2022-05-07 00:11:32 -0300

    add fade transition with svelte transitions

--- src/components/Pokemon.svelte
+++ src/components/Pokemon.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 import type { PokemonType } from "src/types";
+import { fade } from "svelte/transition";
 
 export let pokemon: PokemonType;
 </script>
 
-<a href={`/pokemon/${pokemon?.id}`} class="hover:scale-105 flex-grow duration-300 overflow-hidden decoration-0 w-full flex flex-col justify-center items-center mb-5 border rounded-xl shadow-sm md:h-[200px] md:w-[150px] md:justify-between">
+<a transition:fade href={`/pokemon/${pokemon?.id}`} class="hover:scale-105 flex-grow duration-300 overflow-hidden decoration-0 w-full flex flex-col justify-center items-center mb-5 border rounded-xl shadow-sm md:h-[200px] md:w-[150px] md:justify-between">
     <div class="w-full md:h-[70%] bg-slate-200 flex items-center justify-center">
         <img src={pokemon.image} alt={`${pokemon.name} image`} class="w-[50%] md:w-[70%]">
     </div>
